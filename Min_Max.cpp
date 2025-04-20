469d0218dc6abb01adbe3869acdf027062ec6d53
Prachi Deepak Rane <prachirane7476@gmail.com>
2025-04-20 20:31:42 +0530

    Add files via upload

---
+++ Min_Max.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"Enter 2 Numbers: ";
+cin>>a>>b;
+if(a>b){
+cout<<"the number a is greater that is "<<a;
+}
+else{
+cout<<"the number b is greater that is "<<b;
+}
+
+}
