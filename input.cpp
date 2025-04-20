bf99c85326415a1291caf4cd6cfffacaa86b6fb7
Prachi Deepak Rane <prachirane7476@gmail.com>
2025-04-20 18:32:24 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter a Value of a: ";
+cin>>a;
+}
---
+++ user_input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+int add;
+cout<<"Enter a Value of a: ";
+cin>>a;
+cout<<"Enter a value of b: ";
+cin>>b;
+add=a+b;
+cout<<"The addition of"<<a<<" and "<<b<<" is "<<add<<endl;
+return 0;
+}
